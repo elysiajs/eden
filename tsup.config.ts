1215bc3d124d9530623df8a0879b09b429455e42
Vahelnir <vahelnir@gmail.com>
2024-07-07 23:25:56 +0200

    feat: add treaty2 export

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'tsup'
 import { resolve } from 'path'
 import { execSync } from 'child_process'
 
-import { copyFile } from 'fs/promises'
-
 export default defineConfig({
     entry: {
         index: resolve(__dirname, 'src/index.ts'),
         treaty: resolve(__dirname, 'src/treaty/index.ts'),
+        treaty2: resolve(__dirname, 'src/treaty2/index.ts'),
         fetch: resolve(__dirname, 'src/fetch/index.ts')
     },
     format: ['cjs', 'esm', 'iife'],
